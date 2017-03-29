19a50f0426f565fb4b3e191b5927dd83005a3658
Alex <alex.mahinko@gmail.com>
2017-03-29 16:36:35 -0400

    Player class now has battle system, level up, collect and restart methods

--- exercise3/player.rb
+++ exercise3/player.rb
@@ -1,10 +1,43 @@
 class Player
 
-  def initialize(gold, health, lives)
-    @gold = gold
-    @health = health
-    @lives = lives
-
+  def initialize
+    @gold = 0
+    @health = 10
+    @lives = 5
   end
+
+  def debug
+    puts @gold
+    puts @health
+    puts @lives
+  end
+
+  def level_up
+    @lives += 1
+  end
+
+  def collect_treasure
+    @gold += 1
+    if gold % 10 == 0
+      level_up
+    end
+  end
+
+def battle(damage)
+  @health -= damage
+  @lives -= 1 if @health <= 0
+  @health = 10 if @health <= 0
+  restart if @lives <=0
+end
+
+
+def restart
+  @gold = 0
+  @health = 10
+  @lives = 5
+end
+
+
+
 
 end
